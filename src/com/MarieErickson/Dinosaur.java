a436ae67f56b267f27d5615afdc84464944caf9b
yd7581ku <Malm4614>
2016-09-20 11:31:07 -0500

    added non-arguement method call

---
+++ src/com/MarieErickson/Dinosaur.java
@@ -0,0 +1,14 @@
+package com.MarieErickson;
+
+/**
+ * Created by yd7581ku on 9/20/2016.
+ */
+public class Dinosaur
+{
+    public void roar(){
+    System.out.println("GRRR!!!!!");
+}
+    public void roar(String name){
+        System.out.println(name+" says GRRR!!!!!");
+    }
+}
---
+++ src/com/MarieErickson/DinosaurSupervisor.java
@@ -0,0 +1,14 @@
+package com.MarieErickson;
+
+public class DinosaurSupervisor
+{
+
+    public static void main(String[] args) {
+	// write your code here
+        Dinosaur t_rex = new Dinosaur();
+        Dinosaur velociraptor = new Dinosaur();
+        t_rex.roar("T-Rex");
+        velociraptor.roar("Velociraptor");
+        t_rex.roar();
+    }
+}
